745fe95bff0110f2472b139065c00a9f5ead828d
Pascal Salvador <smv7.github@gmail.com>
2021-11-01 11:16:32 -0300

    Adding basic file structure

---
+++ main.py
@@ -0,0 +1,10 @@
+# This python program receives a compound number from user
+# input and returns a list of the factors of that number.
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
